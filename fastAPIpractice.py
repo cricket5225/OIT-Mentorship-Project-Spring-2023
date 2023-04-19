4a69ecfafdc2177b2a08da89d0751205c2b352da
cricket5225 <111393981+cricket5225@users.noreply.github.com>
2023-04-19 16:52:56 -0400

    Delete fastAPIpractice.py

--- fastAPIpractice.py
+++
@@ -1,9 +0,0 @@
-# FastAPI Practice program
-
-from fastapi import FastAPI # Importing FastAPI Library
-
-app = FastAPI() # app is a FastAPI object
-
-@app.get("/")   # Call decorator on app and HTML request "get"
-def root():     # What does this function do?
-    return { str : "The server is currently up." } # Return string 
