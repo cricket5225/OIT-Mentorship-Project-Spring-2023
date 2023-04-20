d77ddfcea9e48a0a005035345c8ebac9ca5d93be
cricket5225 <111393981+cricket5225@users.noreply.github.com>
2023-04-20 12:22:44 -0400

    Add files via upload

---
+++ ServerCheck.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantReviewProgram.Controllers
+{
+    [ApiController]
+    [Route("[controller]")] 
+    /* Aspect oriented programming(?) Tells what URL should look like to access this-
+     * Route should be name of controller -controller */
+    public class ServerCheck : ControllerBase
+    {
+            private readonly ILogger<ServerCheck> _logger; // Dependency injection
+
+            public ServerCheck(ILogger<ServerCheck> logger)
+            {
+                _logger = logger;
+            }
+
+            [HttpGet()]
+            public string Get()
+            {
+                return "The server is currently up."; //Return string 
+            }
+    }
+}
---
+++ WordCount.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantReviewProgram.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WordCount : ControllerBase
+    {
+        private readonly ILogger<WordCount> _logger;
+
+        public WordCount(ILogger<WordCount> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet()]
+        public RestReview wordCount(string review)
+        {
+            /* There is a duplicate of this in the RestReview
+             * class for its constructor- That returns an int,
+             * this returns a RestReview object */
+            RestReview newRev = new RestReview(review);
+            newRev.RevWordCount = review.Split(' ').Count();
+            return newRev;
+        }
+
+    }
+}
